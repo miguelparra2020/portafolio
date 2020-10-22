fe9edf58dd127406903cb4b65ec16378f405fd93
dinastiadigital <dinastiadigitalecommerce@gmail.com>
2020-10-21 22:39:28 -0500

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { CarruselComponent } from './componentes/carrusel/carrusel.component';
+import { CategoriasComponent } from './componentes/categorias/categorias.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { CategoriasImagenesComponent } from './componentes/categorias-imagenes/categorias-imagenes.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarruselComponent
+    CarruselComponent,
+    CategoriasComponent,
+    HeaderComponent,
+    CategoriasImagenesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/categorias-imagenes/categorias-imagenes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categorias-imagenes',
+  templateUrl: './categorias-imagenes.component.html',
+  styleUrls: ['./categorias-imagenes.component.scss']
+})
+export class CategoriasImagenesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
