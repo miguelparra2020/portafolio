83ae995a4db9c8b56dab37f3702a1751ef262b86
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2020-11-10 23:38:56 -0500

    Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContadorpageComponent } from './componentes/contadorPage/contadorpage.component'
 import { CurriculumPageComponent } from './componentes/curriculum-page/curriculum-page.component';
+import { ElectronicoPageComponent } from './componentes/electronico-page/electronico-page.component';
 import { HomeComponent } from './componentes/home/home.component';
+import { InformaticopageComponent } from './componentes/informaticoPage/informaticopage.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
   { path: 'contador', component: ContadorpageComponent },
+  { path: 'informatico', component: InformaticopageComponent },
+  { path: 'electronico', component: ElectronicoPageComponent },
   { path: 'curriculum', component: CurriculumPageComponent }
 ];
 
